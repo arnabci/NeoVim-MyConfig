301bdff356cc08571913838098b66cda4ce78a67
ArnabCI <94578231+arnabci@users.noreply.github.com>
2023-04-06 23:05:16 -0400

    Delete nvim directory
    old nvim

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require('plugins')
-require('nvim-tree-config')
-require'lspconfig'.pyright.setup{}
--- require("mason-config")setup()
-require("mason-lspconfig").setup()
-require("mason").setup()
-vim.cmd('colorscheme moonfly')
